cb155a5a838a82c3a9695a3b9925f46a7a1038ed
Meharban Singh <meharbansingh10.ms@gmail.com>
2021-03-01 13:50:58 -0600

    Added Transaction class

---
+++ src/Transaction.ts
@@ -0,0 +1,29 @@
+import sha256 from "crypto-js/sha256";
+
+export default class Transaction {
+  /**
+   * Creates a new Transation
+   *
+   * @param {string} fromAddress
+   * @param {string} toAddress
+   * @param {number} amount
+   * @param {number} timestamp
+   */
+  constructor(
+    public fromAddress: string,
+    public toAddress: string,
+    public amount: number,
+    public timestamp: number
+  ) {
+    this.timestamp = Date.now();
+  }
+
+  /**
+   * Creates a SHA256 hash of the current Transaction
+   *
+   * @return {string} hash
+   */
+  calculateHash(): string {
+    return sha256(this.fromAddress + this.toAddress + this.amount).toString();
+  }
+}
